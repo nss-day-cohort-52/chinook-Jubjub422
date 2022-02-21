6c4c1f0dcbe2156f1073dd31004e36ad9af6d099
Jubjub422 <ross.stovenour@gmail.com>
2022-02-21 10:44:53 -0600

    Excersize 3

---
+++ brazil_customer_invoices.sql
@@ -0,0 +1,6 @@
+SELECT c.FirstName, 
+        c.LastName, 
+        i.InvoiceId,
+        i.InvoiceDate, 
+        i.BillingCountry
+    FROM Customer c JOIN Invoice i ON c.CustomerId = i.CustomerId
